8c278427d83cfe93bd442655488a6e63cd98971c
Dylan <dylan.felgenhauer@student.hepl.be>
2022-05-27 14:33:50 +0200

    retour a 0

--- src/ts/Main.ts
+++ src/ts/Main.ts
@@ -1,20 +1,13 @@
-import {Canvas} from "./Canvas";
-import {Ball} from "./Ball";
-import {Animate} from "./Animate";
-import {Score} from "./Score";
+import { Canvas } from "./Canvas";
+
 
 
 class Main{
     canvas:Canvas;
-    animate : Animate;
-    ball: Ball;
-    score : Score;
+
 
     constructor() {
         this.canvas = new Canvas();
-        this.ball = new Ball();
-        this.animate = new Animate();
-        this.score = new Score();
     }
 }
 new Main();
